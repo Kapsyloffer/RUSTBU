d5e0becbc303c3062d27c0421b889a8d55514633
Crilluz <CrilleMega@gmail.com>
2024-01-07 22:45:00 +0100

    Rewrote so that we use enum tile instead of stone structs
    am dum

--- src/game_pieces.rs
+++ src/game_pieces.rs
@@ -1,17 +1,19 @@
-#[allow(unused)]
-#[allow(dead_code)]
+#![allow(dead_code)]
+#![allow(unused)]
+
 pub (crate) struct Board
 {
     color: Color,
     home: Color,
-    state: Vec<Vec<Option<Stone>>>
+    state: [[Tile; 4]; 4]
 }
 
 #[derive(Clone, Copy, PartialEq, Debug)]
-pub (crate) struct Stone
+pub (crate) enum Tile
 {
-    color: Color,
-    position: (usize, usize) 
+    Empty,
+    Black,
+    White
 }
 
 #[derive(Clone, Copy, PartialEq, Debug)]
@@ -35,25 +37,21 @@ impl Board
     }
 
     //Create default board.
-    fn new_state() -> Vec<Vec<Option<Stone>>>
+    fn new_state() -> [[Tile; 4]; 4]
     {
         //Creates a new empty boardstate.
-        let mut board: Vec<Vec<Option<Stone>>> = 
-        vec![
-            vec![None, None, None, None],
-            vec![None, None, None, None],
-            vec![None, None, None, None],
-            vec![None, None, None, None],
-        ];
+        let mut board: [[Tile; 4]; 4] = [[Tile::empty(); 4]; 4];
 
         //Add rocks
         for i in 0..4 
         {
             //Top row (White)
-            board[0][i] = Some(Stone::new(Color::White,  (0, i)));
+            board[0][i] = Tile::white();
+            //board[0][i] = Some(Stone::new(Color::White,  (0, i)));
 
             //Bottom row (Black)
-            board[3][i] = Some(Stone::new(Color::Black,  (3, i)));
+            board[3][i] = Tile::black();
+            //board[3][i] = Some(Stone::new(Color::Black,  (3, i)));
         }
         
         /*
@@ -69,35 +67,150 @@ impl Board
         return board;
     }
 
-    pub fn get_state(&self) -> &Vec<Vec<Option<Stone>>>
+    pub fn get_state(&self) -> &[[Tile; 4]; 4]
     {
         return &self.state;
     }
 
-    pub fn set_state (&mut self, new_state: Vec<Vec<Option<Stone>>>)
+    pub fn set_state (&mut self, new_state: [[Tile; 4]; 4])
     {
         self.state = new_state;
-        self.update_stones();
-    }
-
-    fn update_stones(&mut self)
-    {
-        let state = self.get_state();
-        for i in 0..state.len()
-        {
-            for j in 0..state[i].len()
-            {
-                match state[i][j] 
-                {
-                    Some(mut rock) => rock.set_pos((i, j)),
-                    None => continue,
-                }
-            }
-        }
     }
 }
 
-impl Stone
+impl Tile
+{
+    pub fn empty() -> Tile
+    {
+        return Tile::Empty;
+    }
+
+    pub fn white() -> Tile
+    {
+        return Tile::White;
+    }
+
+    pub fn black() -> Tile
+    {
+        return Tile::Black;
+    }
+
+    pub fn passive_move() -> ()
+    {
+        /*
+        Detta kommer ge typ:
+        [W][B][ ][B]
+        [ ][W][ ][ ]
+        [ ][ ][ ][ ]
+        [ ][w][B][W]
+
+        Om jag väljer lilla w ska jag ha movement i 
+        1x vänster, 1x vänster upp, 1x upp, 2x upphöger, 0x resten.
+        
+        Ta possible moves, 
+        om jag tar en som flyttar 2
+        så får jag size 2 och direction som skickas till aggressive.
+        */
+    }
+
+    pub fn get_possible_moves(&self, b: &Board, aggr: bool, cur_pos: (i8, i8)) -> Vec<(usize, usize)>
+    {
+        let mut boardstate = b.get_state();
+        //let cur_pos = (0,0);//(self.position.0 as i8, self.position.1 as i8); //0 = y, 1 = x
+
+        let mut movelist: Vec<(usize, usize)> = Vec::new();
+        let directions = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, 1), (1, -1)];
+
+        //todo: a move to the left is a [1, 0] 
+        //that gets added to the rock pos if movement.
+
+        for (dy, dx) in directions.iter()
+        {
+            for i in 1..3 as i8
+            {
+                let new_pos = (cur_pos.0 + i * dy, cur_pos.1 + i * dx);
+
+                if self.is_valid(boardstate, cur_pos, new_pos, &i, aggr, (&dy, &dx))
+                {
+                    println!("ADDED {} {}, DIRECTION: {} {}", new_pos.0, new_pos.1, dy, dx);
+                    movelist.push((new_pos.0 as usize, new_pos.1 as usize)); //this is so crummy.
+                    continue;
+                }
+                break;
+            }
+        }
+        return movelist;
+    }
+
+    pub fn is_valid(&self, state: &[[Tile; 4]; 4], cur_pos: (i8, i8), new_pos: (i8, i8), i: &i8, aggr: bool, (dy, dx): (&i8, &i8)) -> bool
+    {
+        //Check if in range.
+        let newy = new_pos.0 as usize;
+        let newx = new_pos.1 as usize;
+
+        let stepy = (cur_pos.0 + 1 * dy) as usize;
+        let stepx = (cur_pos.1  + 1 * dx) as usize;
+
+        //If outta range
+        if newx > 3 || newy > 3
+        {
+            return false;
+        }
+
+        //Passive
+        if !aggr 
+        {
+            if state[newy][newx] != Tile::Empty 
+            {
+                return false;
+            }
+        } 
+        
+        if aggr //Det här är gigacrummy. TODO: Make less crummy.
+        {
+            //Knuffa ej våra egna stenar.
+            if state[newy][newx] == state[cur_pos.0 as usize][cur_pos.1 as usize]
+            {
+                return false;
+            }
+
+            //Om rutan bakom är tom, och rutan har en sten. Move size 1
+            if * i == 1 && state[newy][newx] != Tile::Empty && state[(cur_pos.0 + 2 * dy) as usize][(cur_pos.1 + 2 * dx) as usize] == Tile::Empty
+            {
+                return true;
+            }
+            
+            //Om rutan bakom har en sten, och rutan har en sten. Move size 1
+            if * i == 1 && state[newy][newx] != Tile::Empty && state[(cur_pos.0 + 2 * dy) as usize][(cur_pos.1 + 2 * dx) as usize] != Tile::Empty
+            {
+                return false;
+            }
+
+            //Om rutan bakom har ing en sten, och rutan har ingen sten. Move size 2
+            if state[stepy][stepx] != Tile::Empty && *i == 2 && state[newy][newx] != Tile::Empty
+            {
+                return false;
+            }
+            
+            //Om rutan bakom har ingen sten, och rutan har en sten. Move size 2
+            if state[stepy][stepx] != Tile::Empty && *i == 2 && state[newy][newx] == Tile::Empty
+            {
+                return true;
+            }
+        }
+        return true;
+    }
+
+    pub fn aggressive_move() -> ()
+    {
+        //todo: Somehow kolla vad färgen på tidigare boarden var.
+        //todo: move buffer för passiv och aggressiv?
+    }
+
+}
+
+
+/*impl Stone
 {
     pub (crate) fn new(c: Color, pos: (usize, usize)) -> Stone
     {
@@ -124,118 +237,5 @@ impl Stone
         println!("New position: {} {}", self.get_pos().0, self.get_pos().1);
     }
 
-    pub fn passive_move() -> ()
-    {
-        /*
-        Detta kommer ge typ:
-        [W][B][ ][B]
-        [ ][W][ ][ ]
-        [ ][ ][ ][ ]
-        [ ][w][B][W]
-
-        Om jag väljer lilla w ska jag ha movement i 
-        1x vänster, 1x vänster upp, 1x upp, 2x upphöger, 0x resten.
-        
-        Ta possible moves, 
-        om jag tar en som flyttar 2
-        så får jag size 2 och direction som skickas till aggressive.
-        */
-    }
-
-    pub fn get_possible_moves(&self, b: &Board, aggr: bool) -> Vec<(usize, usize)>
-    {
-        let mut boardstate = b.get_state();
-        let cur_pos = (self.position.0 as i8, self.position.1 as i8); //0 = y, 1 = x
-
-        let mut movelist: Vec<(usize, usize)> = Vec::new();
-        let directions = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, 1), (1, -1)];
-
-        //todo: a move to the left is a [1, 0] 
-        //that gets added to the rock pos if movement.
-
-        for (dy, dx) in directions.iter()
-        {
-            for i in 1..3 as i8
-            {
-                let new_pos = (cur_pos.0 + i * dy, cur_pos.1 + i * dx);
-
-                if self.is_valid(boardstate, cur_pos, new_pos, &i, aggr, (&dy, &dx))
-                {
-                    println!("ADDED {} {}, DIRECTION: {} {}", new_pos.0, new_pos.1, dy, dx);
-                    movelist.push((new_pos.0 as usize, new_pos.1 as usize)); //this is so crummy.
-                    continue;
-                }
-                break;
-            }
-        }
-        return movelist;
-    }
-
-    pub fn is_valid(&self, state: &Vec<Vec<Option<Stone>>>, cur_pos: (i8, i8), new_pos: (i8, i8), i: &i8, aggr: bool, (dy, dx): (&i8, &i8)) -> bool
-    {
-        //Check if in range.
-        let newy = new_pos.0 as usize;
-        let newx = new_pos.1 as usize;
-
-        let stepy = (cur_pos.0 + 1 * dy) as usize;
-        let stepx = (cur_pos.1  + 1 * dx) as usize;
-
-        //If outta range
-        if newx < 0 || newx > 3 || newy < 0 || newy > 3
-        {
-            return false;
-        }
-
-        //Passive
-        if !aggr 
-        {
-            if state[newy][newx].is_some() 
-            {
-                return false;
-            }
-        } 
-        
-        if aggr //Det här är gigacrummy. TODO: Make less crummy.
-        {
-            //Knuffa ej våra egna stenar.
-            if let Some(rock) = state[newy][newx] 
-            {
-                if rock.get_color() == self.get_color() 
-                {
-                    return false; 
-                }
-            }
-
-            //Om rutan bakom är tom, och rutan har en sten. Move size 1
-            if * i == 1 && state[newy][newx].is_some() && state[(cur_pos.0 + 2 * dy) as usize][(cur_pos.1 + 2 * dx) as usize].is_none()
-            {
-                return true;
-            }
-            
-            //Om rutan bakom har en sten, och rutan har en sten. Move size 1
-            if * i == 1 && state[newy][newx].is_some() && state[(cur_pos.0 + 2 * dy) as usize][(cur_pos.1 + 2 * dx) as usize].is_some()
-            {
-                return false;
-            }
-
-            //Om rutan bakom har ing en sten, och rutan har ingen sten. Move size 2
-            if state[stepy][stepx].is_some() && *i == 2 && state[newy][newx].is_some()
-            {
-                return false;
-            }
-            
-            //Om rutan bakom har ingen sten, och rutan har en sten. Move size 2
-            if state[stepy][stepx].is_some() && *i == 2 && state[newy][newx].is_none()
-            {
-                return true;
-            }
-        }
-        return true;
-    }
-
-    pub fn aggressive_move() -> ()
-    {
-        //todo: Somehow kolla vad färgen på tidigare boarden var.
-        //todo: move buffer för passiv och aggressiv?
-    }
-}
+    
+}*/
--- src/game_test.rs
+++ src/game_test.rs
@@ -6,34 +6,30 @@ use crate::game_pieces::*;
 fn state_init()
 {
     let board = Board::new_board(Color::Black, Color::Black);
-    let stone = Stone::new(Color::White, (0,0));
     
-    let top_left_stone = board.get_state()[0][0].as_ref().unwrap();
-    let bot_right_stone = board.get_state()[3][3].as_ref().unwrap();
+    let top_left_stone = board.get_state()[0][0];
+    let bot_right_stone = board.get_state()[3][3];
 
     //White
-    assert_eq!(top_left_stone.get_color(), stone.get_color());
+    assert_eq!(top_left_stone, Tile::White);
     //Black
-    assert_ne!(bot_right_stone.get_color(), stone.get_color());
+    assert_eq!(bot_right_stone, Tile::Black);
 }
 
 #[test]
 fn valid_movement_passive_1()
 {
-    let whitestone = Some(Stone::new(Color::White, (1,2)));
-    let blackstone = Some(Stone::new(Color::Black, (0,2)));
-
-    let boardstate: Vec<Vec<Option<Stone>>> = vec![
-        vec![None, None, blackstone, None],
-        vec![None, None, whitestone, None],
-        vec![None, None, None, None],
-        vec![None, None, None, None],
+    let boardstate: [[Tile; 4]; 4] = [
+        [Tile::Empty, Tile::Empty, Tile::Black, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::White, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty]
     ];
 
     let mut b = Board::new_board(Color::Black, Color::Black);
     b.set_state(boardstate);
 
-    let list = whitestone.unwrap().get_possible_moves(&b, false);
+    let list = b.get_state()[1][2].get_possible_moves(&b, false, (1, 2));
 
     for i in &list
     {
@@ -50,21 +46,17 @@ fn valid_movement_passive_1()
 #[test]
 fn valid_movement_passive_2()
 {
-    let whitestone  = Some(Stone::new(Color::White, (2, 2)));
-    let blackstone  = Some(Stone::new(Color::Black, (0, 2)));
-    let blackstone2 = Some(Stone::new(Color::Black, (3, 2)));
-
-    let boardstate: Vec<Vec<Option<Stone>>> = vec![
-        vec![None, None, blackstone, None],
-        vec![None, None, None, None],
-        vec![None, None, whitestone, None],
-        vec![None, None, blackstone2, None],
+    let boardstate: [[Tile; 4]; 4] = [
+        [Tile::Empty, Tile::Empty, Tile::Black, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::White, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::Black, Tile::Empty]
     ];
 
     let mut b = Board::new_board(Color::Black, Color::Black);
     b.set_state(boardstate);
 
-    let list = whitestone.unwrap().get_possible_moves(&b, false);
+    let list = b.get_state()[2][2].get_possible_moves(&b, false, (2, 2));
 
     assert!(list.contains(&(1, 2)));
 
@@ -77,21 +69,17 @@ fn valid_movement_passive_2()
 #[test]
 fn valid_movement_passive_3()
 {
-    let whitestone  = Some(Stone::new(Color::White, (1, 2)));
-    let blackstone  = Some(Stone::new(Color::Black, (0, 2)));
-    let blackstone2 = Some(Stone::new(Color::Black, (2, 2)));
-
-    let boardstate: Vec<Vec<Option<Stone>>> = vec![
-        vec![None, None, blackstone, None],
-        vec![None, None, whitestone, None],
-        vec![None, None, blackstone2, None],
-        vec![None, None, None, None],
+    let boardstate: [[Tile; 4]; 4] = [
+        [Tile::Empty, Tile::Empty, Tile::Black, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::White, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::Black, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty]
     ];
 
     let mut b = Board::new_board(Color::Black, Color::Black);
     b.set_state(boardstate);
 
-    let list = whitestone.unwrap().get_possible_moves(&b, false);
+    let list = b.get_state()[1][2].get_possible_moves(&b, false, (1, 2));
 
     assert!(!list.contains(&(1, 2)));
     assert!(!list.contains(&(0, 2)));
@@ -102,27 +90,17 @@ fn valid_movement_passive_3()
 #[test]
 fn valid_movement_locked_passive_1()
 {
-    let whitestone  = Some(Stone::new(Color::White, (1, 2)));
-    let blackstone1 = Some(Stone::new(Color::Black, (0, 2)));
-    let blackstone2 = Some(Stone::new(Color::Black, (0, 1)));
-    let blackstone3 = Some(Stone::new(Color::Black, (1, 1)));
-    let blackstone4 = Some(Stone::new(Color::Black, (1, 1)));
-    let blackstone5 = Some(Stone::new(Color::Black, (2, 2)));
-    let blackstone6 = Some(Stone::new(Color::Black, (2, 3)));
-    let blackstone7 = Some(Stone::new(Color::Black, (1, 3)));
-    let blackstone8 = Some(Stone::new(Color::Black, (0, 3)));
-
-    let boardstate: Vec<Vec<Option<Stone>>> = vec![
-        vec![None, blackstone2, blackstone1, blackstone8],
-        vec![None, blackstone3, whitestone , blackstone7],
-        vec![None, blackstone4, blackstone5, blackstone6],
-        vec![None, None, None, None],
+    let boardstate: [[Tile; 4]; 4] = [
+        [Tile::Empty, Tile::Black, Tile::Black, Tile::Black],
+        [Tile::Empty, Tile::Black, Tile::White, Tile::Black],
+        [Tile::Empty, Tile::Black, Tile::Black, Tile::Black],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty]
     ];
 
     let mut b = Board::new_board(Color::Black, Color::Black);
     b.set_state(boardstate);
 
-    let list = whitestone.unwrap().get_possible_moves(&b, false);
+    let list = b.get_state()[1][2].get_possible_moves(&b, false, (1, 2));
 
     assert!(list.is_empty());
 }
@@ -130,54 +108,35 @@ fn valid_movement_locked_passive_1()
 #[test]
 fn valid_movement_locked_passive_2()
 {
-    let whitestone  = Some(Stone::new(Color::White, (1, 2)));
-    let blackstone1 = Some(Stone::new(Color::Black, (0, 2)));
-    let blackstone2 = Some(Stone::new(Color::Black, (0, 1)));
-    let blackstone3 = Some(Stone::new(Color::Black, (1, 0)));
-    let blackstone4 = Some(Stone::new(Color::Black, (1, 1)));
-    let blackstone5 = Some(Stone::new(Color::Black, (2, 2)));
-    let blackstone6 = Some(Stone::new(Color::Black, (2, 3)));
-    let blackstone7 = Some(Stone::new(Color::Black, (1, 3)));
-    let blackstone8 = Some(Stone::new(Color::Black, (0, 3)));
-
-    let boardstate: Vec<Vec<Option<Stone>>> = vec![
-        vec![None, blackstone2, blackstone1, blackstone8],
-        vec![blackstone3, None, whitestone , blackstone7],
-        vec![None, blackstone4, blackstone5, blackstone6],
-        vec![None, None, None, None],
+    let boardstate: [[Tile; 4]; 4] = [
+        [Tile::Empty, Tile::Black, Tile::Black, Tile::Black],
+        [Tile::Black, Tile::Empty, Tile::White, Tile::Black],
+        [Tile::Empty, Tile::Black, Tile::Black, Tile::Black],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty]
     ];
 
     let mut b = Board::new_board(Color::Black, Color::Black);
     b.set_state(boardstate);
 
-    let list = whitestone.unwrap().get_possible_moves(&b, false);
+    let list = b.get_state()[1][2].get_possible_moves(&b, false, (1, 2));
 
     assert_eq!(list[0], (1, 1));
     assert_eq!(list.len(), 1);
 }
 
-
 #[test]
 fn valid_movement_locked_aggressive_1()
 {
-    let whitestone   = Some(Stone::new(Color::White, (0, 3)));
-    let blackstone1  = Some(Stone::new(Color::Black, (0, 2)));
-    let blackstone2  = Some(Stone::new(Color::Black, (1, 2)));
-    let blackstone3  = Some(Stone::new(Color::Black, (1, 3)));
-    let blackstone4  = Some(Stone::new(Color::Black, (0, 1)));
-    let blackstone5  = Some(Stone::new(Color::Black, (2, 3)));
-
-    let boardstate: Vec<Vec<Option<Stone>>> = vec![
-        vec![None, blackstone4, blackstone1,    whitestone],
-        vec![None, None,        blackstone2,    blackstone3],
-        vec![None, None,        None,           blackstone5],
-        vec![None, None,        None,           None],
+    let boardstate: [[Tile; 4]; 4] = [
+        [Tile::Empty, Tile::Black, Tile::Black, Tile::White],
+        [Tile::Empty, Tile::Empty, Tile::Black, Tile::Black],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::White],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty]
     ];
-
     let mut b = Board::new_board(Color::Black, Color::Black);
     b.set_state(boardstate);
 
-    let list = whitestone.unwrap().get_possible_moves(&b, true);
+    let list = b.get_state()[0][3].get_possible_moves(&b, true, (0, 3));
 
     for l in &list
     {
@@ -187,27 +146,20 @@ fn valid_movement_locked_aggressive_1()
     assert!(list.contains(&(2, 1)));
     assert!(list.len() == 2);
 }
-
 #[test]
 fn valid_movement_locked_aggressive_2()
 {
-    let whitestone0  = Some(Stone::new(Color::White, (0, 3)));
-    let whitestone1  = Some(Stone::new(Color::White, (0, 2)));
-    let whitestone2  = Some(Stone::new(Color::White, (1, 2)));
-
-    let blackstone1  = Some(Stone::new(Color::Black, (2, 3)));
-
-    let boardstate: Vec<Vec<Option<Stone>>> = vec![
-        vec![None, None, whitestone1, whitestone0],
-        vec![None, None, whitestone2, None],
-        vec![None, None, None, blackstone1],
-        vec![None, None, None, None],
+    let boardstate: [[Tile; 4]; 4] = [
+        [Tile::Empty, Tile::Empty, Tile::White, Tile::White],
+        [Tile::Empty, Tile::Empty, Tile::White, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Black],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty]
     ];
 
     let mut b = Board::new_board(Color::Black, Color::Black);
     b.set_state(boardstate);
 
-    let list = whitestone0.unwrap().get_possible_moves(&b, true);
+    let list = b.get_state()[0][3].get_possible_moves(&b, true, (0, 3));
 
     for l in &list
     {
@@ -217,26 +169,20 @@ fn valid_movement_locked_aggressive_2()
     assert!(list.contains(&(2, 3)));
     assert_eq!(list.len(), 2);
 }
-
 #[test]
 fn valid_movement_locked_aggressive_3()
 {
-    let whitestone0  = Some(Stone::new(Color::White, (0, 3)));
-    let whitestone1  = Some(Stone::new(Color::White, (0, 2)));
-    let whitestone2  = Some(Stone::new(Color::White, (1, 2)));
-    let whitestone3  = Some(Stone::new(Color::White, (1, 3)));
-
-    let boardstate: Vec<Vec<Option<Stone>>> = vec![
-        vec![None, None, whitestone1, whitestone0],
-        vec![None, None, whitestone2, whitestone3],
-        vec![None, None, None, None],
-        vec![None, None, None, None],
+    let boardstate: [[Tile; 4]; 4] = [
+        [Tile::Empty, Tile::Empty, Tile::White, Tile::White],
+        [Tile::Empty, Tile::Empty, Tile::White, Tile::White],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty]
     ];
 
     let mut b = Board::new_board(Color::Black, Color::Black);
     b.set_state(boardstate);
 
-    let list = whitestone0.unwrap().get_possible_moves(&b, true);
+    let list = b.get_state()[0][3].get_possible_moves(&b, true, (0, 3));
 
     for l in &list
     {
@@ -248,28 +194,18 @@ fn valid_movement_locked_aggressive_3()
 #[test]
 fn valid_movement_locked_aggressive_4()
 {
-    let whitestone0  = Some(Stone::new(Color::White, (0, 3)));
-    let whitestone1  = Some(Stone::new(Color::White, (0, 2)));
-    let whitestone2  = Some(Stone::new(Color::White, (1, 2)));
-    let whitestone3  = Some(Stone::new(Color::White, (1, 3)));
 
-    let black0  = Some(Stone::new(Color::Black, (0, 1)));
-    let black1  = Some(Stone::new(Color::Black, (1, 1)));
-    let black2  = Some(Stone::new(Color::Black, (2, 1)));
-    let black3  = Some(Stone::new(Color::Black, (2, 2)));
-    let black4  = Some(Stone::new(Color::Black, (2, 3)));
-
-    let boardstate: Vec<Vec<Option<Stone>>> = vec![
-        vec![None, black0, whitestone1, whitestone0],
-        vec![None, black1, whitestone2, whitestone3],
-        vec![None, black2, black3, black4],
-        vec![None, None, None, None],
+    let boardstate: [[Tile; 4]; 4] = [
+        [Tile::Empty, Tile::Black, Tile::White, Tile::White],
+        [Tile::Empty, Tile::Black, Tile::White, Tile::White],
+        [Tile::Empty, Tile::Black, Tile::Black, Tile::Black],
+        [Tile::Empty, Tile::Empty, Tile::Empty, Tile::Empty]
     ];
 
     let mut b = Board::new_board(Color::Black, Color::Black);
     b.set_state(boardstate);
 
-    let list = whitestone0.unwrap().get_possible_moves(&b, true);
+    let list = b.get_state()[0][3].get_possible_moves(&b, true, (0, 3));
 
     for l in &list
     {
@@ -281,43 +217,9 @@ fn valid_movement_locked_aggressive_4()
 #[test]
 fn check_if_stones_update()
 {
-    let black0  = Some(Stone::new(Color::Black, (0, 0)));
-    let black1  = Some(Stone::new(Color::Black, (0, 0)));
-    let black2  = Some(Stone::new(Color::Black, (0, 0)));
-    let black3  = Some(Stone::new(Color::Black, (0, 0)));
-    let black4  = Some(Stone::new(Color::Black, (0, 0)));
-
-    let boardstate: Vec<Vec<Option<Stone>>> = vec![
-        vec![None, black0, None, None],
-        vec![None, black1, None, None],
-        vec![None, black2, black3, black4],
-        vec![None, None, None, None],
-    ];
+    let boardstate: [[Tile; 4]; 4] = [[Tile::empty(); 4]; 4];
 
     let mut b = Board::new_board(Color::Black, Color::Black);
 
     b.set_state(boardstate);
-
-    for i in b.get_state()
-    {
-        for j in i
-        {
-            if j.is_some()
-            {
-                println!("{} {}", j.unwrap().get_pos().0, j.unwrap().get_pos().1);
-            }
-        }
-    }
-
-    assert!(b.get_state()[0][1].unwrap().get_pos() != (0, 0));
-    assert!(b.get_state()[1][1].unwrap().get_pos() != (0, 0));
-    assert!(b.get_state()[2][1].unwrap().get_pos() != (0, 0));
-    assert!(b.get_state()[2][2].unwrap().get_pos() != (0, 0));
-    assert!(b.get_state()[2][3].unwrap().get_pos() != (0, 0));
-}
-
-#[test]
-fn set_state_test()
-{
-    let mut b = Board::new_board(Color::Black, Color::Black);
 }
